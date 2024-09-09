bc3d33103f20ade94bf3820178507540f3f22907
Medugu <meduguisaac07@gmail.com>
2024-09-09 11:34:57 +0100

    Add Product to Cart in Javascript

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary" data-product-Id="${product.id}"> 
             Add to Cart
           </button>
         </div>
@@ -53,5 +53,31 @@ products.forEach((product) =>{
           `
         
 })
-console.log(productsHTML)
+// console.log(productsHTML)
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+
+document.querySelectorAll('.add-to-cart-button').forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    const productId = button.dataset.productId
+
+    let matchingItem;
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        matchingItem = item
+      }
+     
+    })
+    if(matchingItem){
+      matchingItem.quantity +=1
+    }else{
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+   
+    console.log(cart)
+  })
+
+})
